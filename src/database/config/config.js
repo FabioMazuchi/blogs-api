cbe7f27e95b5fb8f6d334783a8400afdb1c4d4ae
biozuchi <mazuchi.augusto@hotmail.com>
2025-07-15 13:49:50 -0300

    update

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,32 +1,16 @@
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV || 'test';
-
-const suffix = {
-  dev: '-dev',
-  development: '-dev',
-  test: '-test',
-};
-
-const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'blogs-api'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '1234',
+const config = {
+  username: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || 'password',
+  database: process.env.MYSQLDATABASE || 'blog_api',
+  host: process.env.MYSQLHOST || 'localhost',
+  port: process.env.MYSQLPORT || 3306,
   dialect: 'mysql',
-  dialectOptions: {
-    timezone: 'Z',
-  },
-  logging: process.env.DEBUG !== 'false',
 };
 
 module.exports = {
-  development: {
-    ...options,
-  },
-  test: {
-    ...options,
-  },
+  development: config,
+  test: config,
+  production: config,
 };
