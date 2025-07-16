cc43b11a4248ffc7e6223be807ecf0d30d4f6604
biozuchi <mazuchi.augusto@hotmail.com>
2025-07-16 17:19:34 -0300

    add swagger

--- src/api.js
+++ src/api.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const router = require('./router');
 
 const erroMiddleware = require('./middlewares/errorMiddleware');
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('../swagger.json');
 const loginRouter = require('./login');
 const userRouter = require('./user');
 const categoriesRouter = require('./categories');
 const blogPostRouter = require('./blogPost');
 
 const router = express.Router();
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDoc, { explorer: true }));
 
 router.use('/login', loginRouter);
 router.use('/user', userRouter);
